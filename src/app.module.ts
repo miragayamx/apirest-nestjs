cb12abaeb88a8af70f654e456b547e82d58f6a12
miragayamx <miragaya.m@gmail.com>
2021-08-04 16:13:11 -0300

    base

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductosModule } from './productos/productos.module';
+import { CarritoModule } from './carrito/carrito.module';
+import { UsuarioModule } from './usuario/usuario.module';
 
 @Module({
-  imports: [ProductosModule],
+  imports: [ProductosModule, CarritoModule, UsuarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
